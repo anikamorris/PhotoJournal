6de5dc07d2e5aa20b1aa6cb9d023fa3d46eadad7
Erick Espinoza <wesleyespinoza1021@gmail.com>
2020-04-22 11:00:38 -0400

    Initial Commit

---
+++ PhotoJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournal
+//
+//  Created by Erick Wesley Espinoza on 4/22/20.
+//  Copyright © 2020 HazeStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
